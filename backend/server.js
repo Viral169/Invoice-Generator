e6959513d1dda62659ffe4cb72ecb2c19d6b4168
Viral Baldaniya <viralahir169@gmail.com>
2026-02-22 18:31:25 +0530

    add frontend origin

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://invoice-genera.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
